bf1a60d29f1071767b1a7a93db32a2e574b18f08
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-06-15 16:41:26 +0700

    again add css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,11 +19,11 @@
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Dropdown
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-menu dropdown-menu-fix" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item dropdown-item-fix" href="#">Action</a>
+                        <a class="dropdown-item dropdown-item-fix" href="#">Another action</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                        <a class="dropdown-item dropdown-item-fix" href="#">Something else here</a>
                     </div>
                 </li>
                 <li class="nav-item">
