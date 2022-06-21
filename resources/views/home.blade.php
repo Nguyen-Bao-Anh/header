1cc6f1f6eae5e38f69586bd6999aa9ab1498e8b9
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-06-21 16:29:56 +0700

    #3 fig bug (covensition)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,10 @@
 
             <div class="title_sp">
                 Interactive lessons, "on the go" 
-                <br>practice, peer support
+                <div style="margin-left: 12%;">
+                practice, peer support
+                </div>
+                
             </div>
 
             <div class="start">
@@ -31,5 +34,45 @@
             </div>
 
         </div>
+</div>
+
+<section class="course">
+    <div class="container">
+        <div class="row">
+        <div class="col-lg-4">
+                <div class="card border-course" >
+                    <div class="img-custom"></div>
+                    <div class="card-body card-content">
+                        <h5 class="card-title title-course" >HTML/CSS/js Tutorial</h5>
+                        <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                        <a href="#" class="btn btn-course">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="card border-course" >
+                    <div class="img-custom"></div>
+                    <div class="card-body card-content">
+                        <h5 class="card-title title-course" >HTML/CSS/js Tutorial</h5>
+                        <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                        <a href="#" class="btn btn-course">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 border-course">
+                <div class="card" >
+                    <div class="img-custom"></div>
+                    <div class="card-body card-content">
+                        <h5 class="card-title title-course" >HTML/CSS/js Tutorial</h5>
+                        <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                        <a href="#" class="btn btn-course">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</section>
+   
 @endsection
