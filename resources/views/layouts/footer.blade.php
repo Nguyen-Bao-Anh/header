9da352efa45adb38ef473a00850a78123e120b63
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-06-27 10:37:56 +0700

    "#5 add"

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -74,6 +74,19 @@
                 </div>
 
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 edit-icon">
+<<<<<<< HEAD
+=======
+                    <!-- <h6 class="text-uppercase fw-bold mb-4">
+                        Contact
+                    </h6>
+                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        info@example.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p> -->
+>>>>>>> 1c6eddc (#5 add)
                     <a href="" class="me-4 text-reset edit-icon-private">
                         <i class="fab fa-twitter"></i>
                     </a>
