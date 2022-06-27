72b2243396feac8e5997edad2e314c518eb7ee45
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-06-27 13:39:52 +0700

    #5 fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,7 +40,7 @@
         <div class="row ">
             <div class="col-lg-4 card-distance">
                 <div class="card border-course card-position card-shadow" >
-                    <div class="row no-gutters">
+                    <div class="row no-gutters">img-custotitle-course
                     <div class="img-custom col-lg-12 col-md-4 rounded-top-1"></div>
                     <div class="card-body col-lg-12 col-md-8 card-content">
                         <h5 class="card-title title-course" >HTML/CSS/js Tutorial</h5>
