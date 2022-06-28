8ef213944f56efe8121fdee705ac2ebaee497af8
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-06-28 11:08:55 +0700

    #5 fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,4 @@ mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
     .copy('resources/assets/images', 'public/images')
-    .copy('resources/assets/fonts', 'public/fonts')
+    .copy('resources/assets/fonts', 'public/fonts');
