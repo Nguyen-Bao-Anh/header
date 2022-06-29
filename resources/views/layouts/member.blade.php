f1d796dcd710c014b33291665b284475d0b39d08
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-06-29 09:30:08 +0700

    #7 fix

--- resources/views/layouts/member.blade.php
+++ resources/views/layouts/member.blade.php
@@ -8,3 +8,28 @@
         </div>
     </div>
 </section>
+
+<section class="Statistic">
+    <div class="container-fluid ">
+          <div class="row justify-content-center">
+                <div class="col-lg-2 col-md-3 statistic-title">
+                    Statistic
+                </div>
+            </div>
+
+            <div class="row justify-content-around statistic-location">
+                <div class="col-lg-2 statistic-element">
+                    <p>Courses</p>
+                    <p class="statistic-element-number">1,586</p>
+                </div>
+                <div class="col-lg-2 statistic-element">
+                    <p>Lessons</p>
+                    <p class="statistic-element-number">2,689</p>
+                </div>
+                <div class="col-lg-2 statistic-element">
+                    <p>Learners</p>
+                    <p class="statistic-element-number">16,882</p>
+                </div>
+            </div>
+    </div>
+</section>
