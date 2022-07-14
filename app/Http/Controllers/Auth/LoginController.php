272659d8952e7e48616c25b335772df0e59d858c
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-07-14 15:05:14 +0700

    #15 fix enter line

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Providers\RouteServiceProvider;
+
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
