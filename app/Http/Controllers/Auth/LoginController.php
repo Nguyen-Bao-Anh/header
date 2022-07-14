08297a3fb3355bbd42083b212ee9bd901a9a5044
Nguyen-Bao-Anh <nguyenbaoanh0406@gmail.com>
2022-07-14 14:56:01 +0700

    #15 add login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
-
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     /*
@@ -18,16 +17,13 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
     use AuthenticatesUsers;
-
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-
     /**
      * Create a new controller instance.
      *
@@ -36,5 +32,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = [
+            'username' => $request['username'],
+            'password' => $request['password'],
+        ];
+
+        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        $fieldArr = [
+            $fieldType => $data['username'],
+            'password' => $data['password']
+        ];
+
+        if (Auth::attempt($fieldArr)) {
+            return redirect('/');
+        }
+        return redirect('/login')->with('status', 'Kiểm tra lại User hoặc Password !');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center login-location">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header login-title">{{ __('Sign in to HapoLearn') }}</div>
+
+                @if (session('error'))
+                <div class="alert alert-danger text-center">
+                    {{ session('error') }}
+                </div>
+                @endif
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <div class="col-md-12">
+                                <label for="username" class="col-md-4 col-form-label text-md-left p-0 login-label">{{ __('Username') }}</label>
+                            </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-12">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror login-input" name="username" value="{{ old('username') }}" autocomplete="username" autofocus>
 
-                                @error('email')
+                                @error('username')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -26,10 +34,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <div class="col-md-12">
+                                <label for="password" class="col-md-4 col-form-label text-md-left p-0 login-label">{{ __('Password') }}</label>
+                            </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror login-input" name="password" autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -39,30 +49,47 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-submit">
+                                    {{ __('Sign in') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link forgot-password" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
                             </div>
+                        </div>
+
+                        <div class="form-group row justify-content-center">
+                            <div class="col-md-10 sign-with">
+                                <p class="title-google">Sign in with</p>
+                            </div>
+                        </div>
+
+                        <div class="form-group row justify-content-center mt-4 mb-4">
+                            <div class="col-md-10">
+                                <button class="account-google">
+                                    <a href="#">
+                                        <i class="fa-brands fa-google-plus-g"></i>
+                                        Google
+                                    </a>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="form-group row justify-content-center">
+                            <div class="col-md-10 sign-with model-register">
+                                <p class="title-google title-register">or New to HapoLearn</p>
+                            </div>
+                        </div>
+
+                        <div class="from-group row mt-5 justify-content-center">
+                            <button type="submit" class="btn btn-register">
+                                {{ __('Create New Account') }}
+                            </button>
                         </div>
                     </form>
                 </div>
